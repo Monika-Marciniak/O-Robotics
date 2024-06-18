f70553b1bd9114703d60221997b60550d3efdbec
GuzideGuzelbey <guzide.guzelbey@gmail.com>
2024-06-18 14:04:32 +0200

    pages folder page subfolder and homepage page created

---
+++ src/Pages/Homepage/HomePage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./HomePage.css";
+
+const HomePage = () => {
+  return (
+    <>
+      <section>
+        <div className="home-picture-container"></div>
+      </section>
+    </>
+  );
+};
+export default HomePage;
